c638841072177a621422c36b454211d564d8d128
Artem Yadelskyi <mymmrac@gmail.com>
2023-03-25 20:12:33 +0200

    Added user repository

--- RSSCargo/RSSCargo.DAL/Repositories/Contracts/IUserRepository.cs
+++ RSSCargo/RSSCargo.DAL/Repositories/Contracts/IUserRepository.cs
@@ -5,4 +5,11 @@ using Models;
 public interface IUserRepository
 {
     public User? GetUserByEmail(string email);
+    public void CreateUser(string email, string username, string password);
+    public IEnumerable<UserFeed> GetUserFeeds(int userId);
+    public void AddUserFeed(int userId, string rssFeed);
+    public void RemoveUserFeed(int userId, string rssFeed);
+    public IEnumerable<UserCargo> GetUserCargos(int userId);
+    public void SubscribeUserCargo(int userId, int cargoId);
+    public void UnsubscribeUserCargo(int userId, int cargoId);
 }
--- RSSCargo/RSSCargo.DAL/Repositories/UserRepository.cs
+++ RSSCargo/RSSCargo.DAL/Repositories/UserRepository.cs
@@ -17,4 +17,66 @@ public class UserRepository : IUserRepository
     {
         return _context.Users.FirstOrDefault(u => u.Email == email);
     }
+
+    public void CreateUser(string email, string username, string password)
+    {
+        var user = new User
+        {
+            Email = email,
+            Username = username,
+            Password = password,
+        };
+
+        _context.Users.Add(user);
+    }
+
+    public IEnumerable<UserFeed> GetUserFeeds(int userId)
+    {
+        return _context.UserFeeds.Where(f => f.UserId == userId);
+    }
+
+    public void AddUserFeed(int userId, string rssFeed)
+    {
+        var feed = new UserFeed
+        {
+            UserId = userId,
+            RssFeed = rssFeed,
+        };
+        _context.UserFeeds.Add(feed);
+    }
+
+    public void RemoveUserFeed(int userId, string rssFeed)
+    {
+        var feed = new UserFeed
+        {
+            UserId = userId,
+            RssFeed = rssFeed,
+        };
+        _context.UserFeeds.Remove(feed);
+    }
+
+    public IEnumerable<UserCargo> GetUserCargos(int userId)
+    {
+        return _context.UserCargos.Where(c => c.UserId == userId);
+    }
+
+    public void SubscribeUserCargo(int userId, int cargoId)
+    {
+        var cargo = new UserCargo()
+        {
+            UserId = userId,
+            CargoId = cargoId,
+        };
+        _context.UserCargos.Add(cargo);
+    }
+
+    public void UnsubscribeUserCargo(int userId, int cargoId)
+    {
+        var cargo = new UserCargo()
+        {
+            UserId = userId,
+            CargoId = cargoId,
+        };
+        _context.UserCargos.Remove(cargo);
+    }
 }
