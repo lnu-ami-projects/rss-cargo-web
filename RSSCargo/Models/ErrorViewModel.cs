2726432b8331e621bdeb43f70d18b4a061951a00
Likor7 <sov30082003@gmail.com>
2023-03-26 09:00:22 +0300

    Changed location of folders and files

--- RSSCargo/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace RSSCargo.PL.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ RSSCargo/RSSCargo.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RSSCargo.PL.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
