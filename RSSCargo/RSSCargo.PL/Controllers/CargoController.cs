7ec0c488149dd81e10e846032d63ce764b2bd247
khiletska <hiletskak@gmail.com>
2023-04-03 21:05:54 +0300

    Add view

---
+++ RSSCargo/RSSCargo.PL/Controllers/CargoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RSSCargo.PL.Controllers
+{
+    public class CargoController : Controller
+    {
+        public IActionResult Cargo()
+        {
+            return View();
+        }
+    }
+}
---
+++ RSSCargo/RSSCargo.PL/Controllers/FeedController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RSSCargo.PL.Controllers
+{
+    public class FeedController : Controller
+    {
+        public IActionResult Feed()
+        {
+            return View();
+        }
+        public IActionResult AddFeed()
+        {
+            return View();
+        }
+    }
+}
